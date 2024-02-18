797f24ace107371f2be9933ccc96b917ece88322
CRY-TeX <blacklion.x0@gmail.com>
2024-02-18 04:33:49 +0100

    wave function

--- scripts/WaterLevel.gd
+++ scripts/WaterLevel.gd
@@ -11,31 +11,20 @@ const LEVEL_LENGTH = 1024.0 * 2
 var points: Array = []
 var current_point: int = 0
 
+func create_sin_curve(start_point: Vector2, end_point: Vector2):
+	var amplitude = rand_range(50, 100)
+	var frequency = rand_range(0.005, 0.01)
+	var points = []
+	for i in range(0, LEVEL_LENGTH):
+		amplitude = rand_range(50, 100)
+		var x = lerp(start_point.x, end_point.x, i / LEVEL_LENGTH)
+		var y = sin(x * frequency) * amplitude
+		points.append(Vector2(x, y))
+	return points
+
+
 func _ready():
-	# for i in range(0, LEVEL_LENGTH, 80):
-	# 	var t = i / LEVEL_LENGTH
-	# 	var p0 = Vector2(i, rand_range(MIN_WATER_LEVEL, MAX_WATER_LEVEL))
-	# 	var p1 = Vector2(i + 512, rand_range(MIN_WATER_LEVEL, MAX_WATER_LEVEL))
-	# 	var p2 = Vector2(i + 1024, rand_range(MIN_WATER_LEVEL, MAX_WATER_LEVEL))
-	# 	points.append(bezier(t, p0, p1, p2))
-
-	# print(points.size())
-
-	# Water.draw_water(points, points[points.size() - 1].x)
-
-func _process(delta):
-	pass
-	# var target_point = points[current_point]
-	# var direction = (target_point - LifeBoat.position).normalized()
-	# var velocity = direction * 150 * delta
-
-	# LifeBoat.position += velocity
-	# Camera.position.x += velocity.x
-
-	# if LifeBoat.position.distance_to(target_point) < 1:
-	#     current_point += 1
-	#     if current_point >= points.size():
-	#         current_point = 0
-		
-
-	
+	var start_point = Vector2(0, MIN_WATER_LEVEL)
+	var end_point = Vector2(LEVEL_LENGTH, MIN_WATER_LEVEL)
+	points = create_sin_curve(start_point, end_point)
+	Water.draw_water(points, points[points.size() - 1].x)
